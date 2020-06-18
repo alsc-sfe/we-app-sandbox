91173ba3916c21a214b4420b446c37024294a462
zetay.lim <zetay.lim@alibaba-inc.com>
2020-06-18 11:45:49 +0800

    fix: document 方法 proxy

--- src/ui-sandbox/document-proxy.ts
+++ src/ui-sandbox/document-proxy.ts
@@ -6,50 +6,38 @@ import Sandbox from '..';
 export default function makeDocumentProxy(shadowDocument: ShadowDocument, sandbox: Sandbox, opts: any) {
   return new Proxy(shadowDocument, {
     get(target, key) {
+      // 拦截元素查询方法，保证html、head、body能够正确返回
+      if (key === 'querySelector') {
+        return function (selector) {
+          if (isRootSelector(selector)) {
+            return target.body;
+          }
+          return target[key].call(target, selector);
+        };
+      }
+      if (['querySelectorAll', 'getElementsByTagName'].indexOf(key as string) > -1) {
+        return function (selector) {
+          if (isRootSelector(selector)) {
+            return [target.body];
+          }
+          return target[key].call(target, selector);
+        };
+      }
+
+      // 拦截元素注入方法，拦截script，并在沙箱中运行
+      // link[stylesheet]、style无需拦截，因为html、head、body已经被修正
+      if (key === 'appendChild') {
+        return function (element: HTMLElement) {
+          const el = intercept(element, sandbox, opts);
+          return target[key].call(target, el);
+        };
+      }
+
       if (!target[key]) {
-        if (typeof document[key] === 'function') {
-          // 拦截元素查询方法，保证html、head、body能够正确返回
-          if (key === 'querySelector') {
-            return function (selector) {
-              if (isRootSelector(selector)) {
-                return target.body;
-              }
-              return target[key].call(target, selector);
-            };
-          }
-          if (['querySelectorAll', 'getElementsByTagName'].indexOf(key as string) > -1) {
-            return function (selector) {
-              if (isRootSelector(selector)) {
-                return [target.body];
-              }
-              return target[key].call(target, selector);
-            };
-          }
-
-          // 拦截元素注入方法，拦截script，并在沙箱中运行
-          // link[stylesheet]、style无需拦截，因为html、head、body已经被修正
-          if (key === 'appendChild') {
-            return function (element: HTMLElement) {
-              const el = intercept(element, sandbox, opts);
-              return target[key].call(target, el);
-            };
-          }
-
-          if (key === 'addEventListener') {
-            console.log('document.addEventListener');
-          }
-
-          // 避免存在静态属性的方法在经过bind之后丢失静态属性
-          return getTargetValue(document, document[key]);
-        }
-
-        return document[key];
+        return getTargetValue(document, document[key]);
       }
 
-      if (typeof target[key] === 'function') {
-        return target[key].bind(target);
-      }
-      return target[key];
+      return getTargetValue(target, target[key]);
     },
   });
 }
--- src/ui-sandbox/util.ts
+++ src/ui-sandbox/util.ts
@@ -40,23 +40,27 @@ export function isBoundedFunction(fn: CallableFunction) {
 const boundValueSymbol = Symbol('bound value');
 
 export function getTargetValue(target: any, value: any): any {
+  if (typeof value === 'function') {
+    return value.bind(target);
+  }
+  return value;
   /*
     仅绑定 isCallable && !isBoundedFunction && !isConstructable 的函数对象，如 window.console、window.atob 这类。目前没有完美的检测方式，这里通过 prototype 中是否还有可枚举的拓展方法的方式来判断
     @warning 这里不要随意替换成别的判断方式，因为可能触发一些 edge case（比如在 lodash.isFunction 在 iframe 上下文中可能由于调用了 top window 对象触发的安全异常）
    */
-  if (isCallable(value) && !isBoundedFunction(value) && !isConstructable(value)) {
-    if (value[boundValueSymbol]) {
-      return value[boundValueSymbol];
-    }
+  // if (isCallable(value) && !isBoundedFunction(value) && !isConstructable(value)) {
+  //   if (value[boundValueSymbol]) {
+  //     return value[boundValueSymbol];
+  //   }
 
-    const boundValue = value.bind(target);
-    // some callable function has custom fields, we need to copy the enumerable props to boundValue. such as moment function.
-    Object.keys(value).forEach(key => {
-      boundValue[key] = value[key];
-    });
-    Object.defineProperty(value, boundValueSymbol, { enumerable: false, value: boundValue });
-    return boundValue;
-  }
+  //   const boundValue = value.bind(target);
+  //   // some callable function has custom fields, we need to copy the enumerable props to boundValue. such as moment function.
+  //   Object.keys(value).forEach(key => {
+  //     boundValue[key] = value[key];
+  //   });
+  //   Object.defineProperty(value, boundValueSymbol, { enumerable: false, value: boundValue });
+  //   return boundValue;
+  // }
 
-  return value;
+  // return value;
 }
